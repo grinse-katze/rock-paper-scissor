37b9d6380688385b1011d1220fe8d12e35b0377e
grinsekatze <violetta.stix@gmail.com>
2022-03-28 16:00:47 +0200

    - HTML, JS & Style files added - HTML skeleton added - JS early testing

---
+++ main.js
@@ -0,0 +1,20 @@
+const availableElements = ['rock', 'paper', 'scissor'];
+const randomElement = Math.floor(Math.random() * availableElements.length);
+console.log(randomElement, availableElements[randomElement]);
+
+
+
+/*function playRound(playerSelection, computerSelection) {
+    //randomly return Rock, Paper or Scissor
+}
+
+
+function computerPlay(){
+    //randomly return Rock, Paper or Scissor
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+*/
