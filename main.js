67c5876dc187e887837c8d016738329a4ac5a7f0
grinsekatze <violetta.stix@gmail.com>
2022-03-28 21:33:26 +0200

    Scoreboard added

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
-const availableElements = ['rock', 'paper', 'scissor'];
+const choices = ['rock', 'paper', 'scissor'];
 
-
-let computerSelection;
-let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay(){
     //randomly return Rock, Paper or Scissor
-    const randomElement = Math.floor(Math.random() * availableElements.length);
-    return availableElements[randomElement];
+    const randomElement = Math.floor(Math.random() * choices.length);
+    return choices[randomElement];
 }
 
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    //check if user input is in the availableElements array
-    if (playerSelection == availableElements[0] || 
-        playerSelection == availableElements[1] || 
-        playerSelection == availableElements[2]) {
+
+    //check if user input is in the choices array
+    if (playerSelection == choices[0] || 
+        playerSelection == choices[1] || 
+        playerSelection == choices[2]) {
         
         if (playerSelection == computerSelection){
             alert(`${playerSelection} ties with ${computerSelection}`);
-        } else if (playerSelection == availableElements[0] && computerSelection == availableElements[2]){
-            //if user chooses rock against scissor = user wins
+        } else if (
+            (playerSelection == choices[0] && computerSelection == choices[2]) ||
+            (playerSelection == choices[2] && computerSelection == choices[1]) ||
+            (playerSelection == choices[1] && computerSelection == choices[0])
+            ){
             alert(`${playerSelection} wins against ${computerSelection}`);
-        } else if (playerSelection == availableElements[2] && computerSelection == availableElements[1]){
-            //if user chooses scissor against paper = user wins
-            alert(`${playerSelection} wins against ${computerSelection}`);
-        } else if (playerSelection == availableElements[1] && computerSelection == availableElements[0]){
-            //if user chooses paper against rock = user wins
-            alert(`${playerSelection} wins against ${computerSelection}`);
-        }
-        else{
+            playerScore += 1;
+        } else{
             alert(`${playerSelection} loses against ${computerSelection}`);
+            computerScore += 1;
         }
+        alert(`Current score: 
+                You: ${playerScore}
+                Computer: ${computerScore}`);
 
     } else{
         alert("Please choose one of the following: rock, paper, scissor");
@@ -41,10 +42,17 @@ function playRound(playerSelection, computerSelection){
 
 function game(){
     for(let i = 0; i < 5; i++) {
+        let computerSelection;
+        let playerSelection;
         playerSelection = prompt("Choose one of the following: rock, paper, scissor");
         computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);
+        
     }
+}
+
+function playerCount(){
+
 }
 
 game();
