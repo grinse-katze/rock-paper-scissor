ec8b83ff3d01428c2b7be2c9389d78cf15b3a165
grinsekatze <violetta.stix@gmail.com>
2022-03-28 18:03:31 +0200

    - Update

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const availableElements = ['rock', 'paper', 'scissor'];
-const randomElement = Math.floor(Math.random() * availableElements.length);
-console.log(randomElement, availableElements[randomElement]);
-
-
-
-/*function playRound(playerSelection, computerSelection) {
-    //randomly return Rock, Paper or Scissor
-}
-
 
 function computerPlay(){
     //randomly return Rock, Paper or Scissor
+    const randomElement = Math.floor(Math.random() * availableElements.length);
+    return availableElements[randomElement];
 }
 
-const playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection){
+    //check if user inout is in the availableElements array
+    if (playerSelection == availableElements[0] || playerSelection == availableElements[1] || playerSelection == availableElements[2]) {
+        if (playerSelection == computerSelection){
+            console.log("It's a tie!");
+        } else if (playerSelection > computerSelection){
+            console.log("You win!");
+        } else{
+            console.log("You lose!");
+        }
+    } else{
+        console.log("Please choose one of the following: rock, paper, scissor");
+    }
+}
+
 const computerSelection = computerPlay();
+const playerSelection = "rock";
 console.log(playRound(playerSelection, computerSelection));
 
-*/
