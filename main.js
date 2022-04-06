57bff032ff26018388c24ff99788ec231ee1e39c
grinsekatze <violetta.stix@gmail.com>
2022-04-06 16:48:27 +0200

    - Code Cleanup - Gameover added

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const buttons = document.querySelectorAll('button');
-const pScore = document.querySelector('#player-score');
-const cScore = document.querySelector('#computer-score');
 
 let playerScore = 0;
 let computerScore = 0;
-
-
-//document.querySelector('#status').textContent = "";
-
-
 
 function computerPlay(){
     const choices = ['rock', 'paper', 'scissor'];
@@ -16,56 +9,57 @@ function computerPlay(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+function playRound(playerSelection, computerSelection){      
+    let status = '';
+
+    if (playerScore < 5 && computerScore < 5){
+        if (playerSelection == computerSelection){
+            status = `${playerSelection} ties with ${computerSelection}`;
+        } else if (
+            (playerSelection == 'rock' && computerSelection == 'scissor') ||
+            (playerSelection == 'scissor' && computerSelection == 'paper') ||
+            (playerSelection == 'paper' && computerSelection == 'rock')
+            ){
+            status = `${playerSelection} wins against ${computerSelection}`;
+            playerScore += 1;
+        } else{
+            status = `${playerSelection} loses against ${computerSelection}`;
+            computerScore += 1;
+        }
+    
+        // Update Status & Score in document
+        document.querySelector('#status').textContent = status;
+        document.querySelector('#player-score').textContent = playerScore;
+        document.querySelector('#computer-score').textContent = computerScore;
+    } 
+
+    else {
+        gameOver();
+    }
+}
+
+function gameOver(){
+    if (playerScore === 5) {
+        document.querySelector('#status').textContent = "You win!";
+    } else {
+        document.querySelector('#status').textContent = "You lose!";
+    }
+}
+
+/*
+function game(userInput){
+    playRound(userInput, computerPlay())
+}
+*/
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        playRound(button.id);
-    })
-})
+        return playRound(button.id, computerPlay());
+    });
+});
 
 
 
-function playRound(playerSelection, computerSelection){      
-    computerSelection = computerPlay();
-    let status = '';  
-    if (playerSelection == computerSelection){
-        status = `${playerSelection} ties with ${computerSelection}`;
-    } else if (
-        (playerSelection == 'rock' && computerSelection == 'scissor') ||
-        (playerSelection == 'scissor' && computerSelection == 'paper') ||
-        (playerSelection == 'paper' && computerSelection == 'rock')
-        ){
-        status = `${playerSelection} wins against ${computerSelection}`;
-        playerScore += 1;
-    } else{
-        status = `${playerSelection} loses against ${computerSelection}`;
-        computerScore += 1;
-    }
-    // Update Status & Score text in document
-    document.querySelector('#status').textContent = status;
-    document.querySelector('#player-score').textContent = playerScore;
-    document.querySelector('#computer-score').textContent = computerScore;
-}
-
-
-
-/*
-
-
-function game(){
-    for(let i = 0; i < 5; i++) {
-        let computerSelection;
-        let playerSelection;
-        playerSelection = prompt("Choose one of the following: rock, paper, scissor");
-        computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-        
-    }
-}
-
-game();
-
-*/
 
 
 
